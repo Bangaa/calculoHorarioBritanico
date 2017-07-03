50004ea3bb4b2ee4f0232ef6a2ca49cb8572c01b
Ian Mejias <idmconejeros@gmail.com>
2017-07-03 03:39:21 -0400

    Refactor a la clase Horario.
    Se hacen privados algunos atributos y se elimina un método que no
    correspondía a la clase.

--- app/Horario.py
+++ app/Horario.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-from datetime import date, time, timedelta
+from datetime import time
 import re
 
 class Horario:
@@ -14,47 +14,38 @@ class Horario:
     # @param initial La hora inicial en formato "militar"
     # @param final La hora final en formato "militar"
     def __init__(self, weekday, initial, final):
-        self.hora_i = time(initial/100, initial%100)
-        self.hora_f = time(final/100, final%100)
-
-        mfinal = final/100 * 60 + final%100
-        minitial = initial/100 * 60 + initial%100
-        self.duration = mfinal - minitial # cantidad de minutos del horario
+        self.__hora_i = time(initial/100, initial%100)
+        self.__hora_f = time(final/100, final%100)
 
         self.weekday = weekday
 
+    def duracion(self):
+        mfinal = self.__hora_f.hour * 60 + self.__hora_f.minute
+        minitial = self.__hora_i.hour * 60 + self.__hora_i.minute
+        duration = mfinal - minitial # cantidad de minutos del horario
+
+        return duration
+
     def __lt__(self, horario):
         d = self.weekday - horario.weekday
-        if d < 0 or self.hora_i < horario.hora_i and d == 0:
+        if d < 0 or self.__hora_i < horario.__hora_i and d == 0:
             return True
         return False
 
     def __gt__(self, horario):
         d = self.weekday - horario.weekday
-        if d > 0 or self.hora_i > horario.hora_i and d == 0:
+        if d > 0 or self.__hora_i > horario.__hora_i and d == 0:
             return True
         return False
 
     def __eq__(self, horario):
         d = self.weekday - horario.weekday
-        if d == 0 and self.hora_i == horario.hora_i and self.hora_f == horario.hora_f:
+        if d == 0 and self.__hora_i == horario.__hora_i and self.__hora_f == horario.__hora_f:
             return True
         return False
 
     def __str__(self):
         dia = ["lunes", "martes", "miercoles", "jueves", "viernes", "sabado", "domingo"]
-        return "%9s de %s a %s" % (dia[self.weekday], self.hora_i.isoformat(), self.hora_f.isoformat())
+        return "%9s de %s a %s" % (dia[self.weekday], self.__hora_i.isoformat(), self.__hora_f.isoformat())
 
-    # devuelve la proxima fecha en la cual se pueda cumplir el horario
-    def sig_clase(self, afterdate = date.today()):
-        difdias = afterdate.weekday() - self.weekday
-
-        if difdias > 0:
-            delay = timedelta(days=6-afterdate.weekday()+self.weekday+1)
-        elif difdias < 0:
-            delay = timedelta(days=-difdias)
-        else:
-            delay = timedelta()
-
-        return afterdate + delay
 
